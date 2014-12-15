4b882b6a8dcf4b1df82e515902dbd6d24552bbb7
cristina sanchez <cristina.s.narbon@gmail.com>
2014-12-15 13:19:50 +0100

    get usuario

--- photo-api/src/main/java/edu/upc/eetac/dsa/abaena/photo/api/UserResource.java
+++ photo-api/src/main/java/edu/upc/eetac/dsa/abaena/photo/api/UserResource.java
@@ -1,5 +1,86 @@
 package edu.upc.eetac.dsa.abaena.photo.api;
 
-public class UserResource {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.core.Response;
+
+import edu.upc.eetac.dsa.abaena.photo.api.DataSourceSPA;
+import edu.upc.eetac.dsa.abaena.photo.api.MediaType;
+import edu.upc.eetac.dsa.abaena.photo.api.model.User;
+
+@Path("/users")
+public class UserResource {
+	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
+	
+	
+	private final static String GET_USER_BY_USERID = "Select * from Users where userid=?";
+	
+	
+	@GET 
+	@Path("/{userid}")
+	@Produces(MediaType.PHOTO_API_USER)
+	public User getUser(@PathParam("userid")int userid)  {
+		
+		User user = new User();
+		
+		Connection conn = null;
+		
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+	 
+		PreparedStatement stmt = null;
+		try {
+			stmt = conn.prepareStatement(GET_USER_BY_USERID);
+			stmt.setInt(1,Integer.valueOf (userid));
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				user.setUserid(rs.getInt("userid"));
+				user.setUsername(rs.getString("username"));
+				user.setPassword(rs.getString("password"));
+				user.setAvatar(rs.getInt("avatar"));
+				user.setIdprofile(rs.getInt("idprofile"));
+				
+			}
+			else {
+				throw new NotFoundException("No existe ningún usuario con este userid="
+				+ userid);
+				}
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+		
+		return user;
+	}
+	
 }
+		
+
+
+		
+
