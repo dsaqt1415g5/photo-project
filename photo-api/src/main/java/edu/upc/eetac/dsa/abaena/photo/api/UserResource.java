970151d116d04c8d3ca3dfc8af7cb16a5674a6d0
cristina sanchez <cristina.s.narbon@gmail.com>
2014-12-17 13:21:59 +0100

    sql cambiado (sin userid), crear usuario

--- photo-api/src/main/java/edu/upc/eetac/dsa/abaena/photo/api/UserResource.java
+++ photo-api/src/main/java/edu/upc/eetac/dsa/abaena/photo/api/UserResource.java
@@ -4,19 +4,22 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
 
-
-
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import edu.upc.eetac.dsa.abaena.photo.api.DataSourceSPA;
 import edu.upc.eetac.dsa.abaena.photo.api.MediaType;
@@ -25,44 +28,40 @@ import edu.upc.eetac.dsa.abaena.photo.api.model.User;
 @Path("/users")
 public class UserResource {
 	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
-	
-	
-	private final static String GET_USER_BY_USERID = "Select * from Users where userid=?";
-	
-	
-	@GET 
-	@Path("/{userid}")
+
+	private final static String GET_USER_BY_USERNAME = "Select * from Users where username=?";
+	private final static String INSERT_USER_INTO_USERS = "insert into Users (username, password, avatar) values(?, MD5(?), null)";
+
+	@GET
+	@Path("/{username}")
 	@Produces(MediaType.PHOTO_API_USER)
-	public User getUser(@PathParam("userid")int userid)  {
-		
+	public User getUser(@PathParam("username") String username) {
+
 		User user = new User();
-		
+
 		Connection conn = null;
-		
+
 		try {
 			conn = ds.getConnection();
 		} catch (SQLException e) {
 			throw new ServerErrorException("Could not connect to the database",
 					Response.Status.SERVICE_UNAVAILABLE);
 		}
-	 
+
 		PreparedStatement stmt = null;
 		try {
-			stmt = conn.prepareStatement(GET_USER_BY_USERID);
-			stmt.setInt(1,Integer.valueOf (userid));
+			stmt = conn.prepareStatement(GET_USER_BY_USERNAME);
+			stmt.setString(1, username);
 			ResultSet rs = stmt.executeQuery();
 			if (rs.next()) {
-				user.setUserid(rs.getInt("userid"));
 				user.setUsername(rs.getString("username"));
 				user.setPassword(rs.getString("password"));
 				user.setAvatar(rs.getInt("avatar"));
-				user.setIdprofile(rs.getInt("idprofile"));
-				
+
+			} else {
+				throw new NotFoundException(
+						"No existe ningún usuario con este username=" + username);
 			}
-			else {
-				throw new NotFoundException("No existe ningún usuario con este userid="
-				+ userid);
-				}
 		} catch (SQLException e) {
 			throw new ServerErrorException(e.getMessage(),
 					Response.Status.INTERNAL_SERVER_ERROR);
@@ -74,13 +73,79 @@ public class UserResource {
 			} catch (SQLException e) {
 			}
 		}
-		
+		user.setPassword(null);
 		return user;
 	}
-	
+
+	@POST
+	@Consumes(MediaType.PHOTO_API_USER)
+	@Produces(MediaType.PHOTO_API_USER)
+	public User CreateUser(User user) {
+
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+
+		PreparedStatement stmtGetUsername = null;
+		PreparedStatement stmtGetPassword = null;
+		PreparedStatement stmtInsertUserIntoUsers = null;
+
+		try {
+			stmtGetUsername = conn.prepareStatement(GET_USER_BY_USERNAME);
+			stmtGetUsername.setString(1, user.getUsername());
+
+			ResultSet rs = stmtGetUsername.executeQuery();
+			if (rs.next())
+				throw new WebApplicationException(user.getUsername()
+						+ " El usuario ya existe.", Status.CONFLICT);// tu
+																		// nombre
+																		// de
+																		// usuario
+																		// ya
+																		// existe
+			rs.close();
+
+			conn.setAutoCommit(false);// desactivamos las confirmaciones
+										// automaticas
+			stmtInsertUserIntoUsers = conn
+					.prepareStatement(INSERT_USER_INTO_USERS);
+
+			stmtInsertUserIntoUsers.setString(1, user.getUsername());
+			stmtInsertUserIntoUsers.setString(2, user.getPassword());
+
+			stmtInsertUserIntoUsers.executeUpdate();
+
+			conn.commit();
+
+		}
+
+		catch (SQLException e) {
+			if (conn != null)
+				try {
+					conn.rollback();
+				} catch (SQLException e1) {
+				}
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmtGetUsername != null)
+					stmtGetUsername.close();
+
+				if (stmtInsertUserIntoUsers != null)
+					stmtGetUsername.close();
+				conn.setAutoCommit(true);
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+
+		user.setPassword(null);
+		return user;
+	}
+
 }
-		
-
-
-		
-
--- photo-api/src/main/java/edu/upc/eetac/dsa/abaena/photo/api/model/User.java
+++ photo-api/src/main/java/edu/upc/eetac/dsa/abaena/photo/api/model/User.java
@@ -2,18 +2,13 @@ package edu.upc.eetac.dsa.abaena.photo.api.model;
 
 public class User {
 	
-	private int userid;
+	
 	private String username;
 	private String password;
 	private int avatar;
-	private int idprofile;
 	
-	public int getUserid() {
-		return userid;
-	}
-	public void setUserid(int userid) {
-		this.userid = userid;
-	}
+	
+
 	public String getUsername() {
 		return username;
 	}
@@ -32,12 +27,7 @@ public class User {
 	public void setAvatar(int avatar) {
 		this.avatar = avatar;
 	}
-	public int getIdprofile() {
-		return idprofile;
-	}
-	public void setIdprofile(int idprofile) {
-		this.idprofile = idprofile;
-	}
+
 
 	
 }
