bd0af170f5924f38df5e72dd0d1ede30d59c4b41
cristina sanchez <cristina.s.narbon@gmail.com>
2014-12-17 13:33:02 +0100

    sql cambiado (sin userid) y crear usuario funcionando

--- photo-api/src/main/java/edu/upc/eetac/dsa/abaena/photo/api/UserResource.java
+++ photo-api/src/main/java/edu/upc/eetac/dsa/abaena/photo/api/UserResource.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.sql.DataSource;
-
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
@@ -148,4 +148,8 @@ public class UserResource {
 		return user;
 	}
 
+/*	@DELETE
+	@Path("/{username")
+	public void deleteUser*/
+	
 }
